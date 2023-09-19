03a3ed0237ca097dd43255a16b2f613a55e0e488
abhi <nath.rs@icloud.com>
2023-09-19 19:47:03 +1000

    rest api call

---
+++ firstPrj/NewsAPI.py
@@ -0,0 +1,6 @@
+import requests
+
+r = requests.get('https://newsapi.org/v2/everything?qInTitle=stock%20market&from=2023-9-18&to=2023-9-19&sortBy=popularity&language=en&apiKey=fc1af8447bdf4f9c9064318c48a77588')
+content = r.json()
+#print(type(content))
+print(content['articles'][0]['title'])
